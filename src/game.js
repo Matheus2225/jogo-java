7967fb9a46ed8402056fea2cdcd645958f23dab4
Matheus2225 <123407668+Matheus2225@users.noreply.github.com>
2025-05-14 11:46:23 -0300

    .

---
+++ src/game.js
@@ -0,0 +1,19 @@
+JavaScript
+
+import GameScene from './scenes/GameScene.js';
+
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 1200 },
+            debug: false
+        }
+    },
+    scene: [GameScene]
+};
+
+const game = new Phaser.Game(config);
--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,25 +1,5 @@
 JavaScript
 
-import GameScene from './scenes/GameScene.js';
-
-const config = {
-    type: Phaser.AUTO,
-    width: 800,
-    height: 600,
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: { y: 1200 },
-            debug: false
-        }
-    },
-    scene: [GameScene]
-};
-
-const game = new Phaser.Game(config);
-
-JavaScript
-
 class GameScene extends Phaser.Scene {
     constructor() {
         super({ key: 'GameScene' });
